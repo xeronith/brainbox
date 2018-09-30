36cd4824860695d07df92da9e12b0c2c2a3699ae
Andreas Herz <a.herz@freegroup.de>
2018-09-30 15:14:56 +0200

    help file

---
+++ regenrate.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+# find -name '*.rar' -exec unrar x {} \;
+
+
+/Users/d023280/Documents/workspace/brainbox/node_modules/phantomjs/lib/phantom/bin/phantomjs  /Users/d023280/Documents/workspace/brainbox/app/shape2code/converter.js $1 /Users/d023280/Documents/workspace/brainbox/app/shape2code/ /Users/d023280/Documents/workspace/brainbox/app/shapes/
