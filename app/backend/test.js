94d0cac36fc4ac3e721d177a373a6d340adcd48c
Herz, Andreas <a.herz@freegroup.de>
2020-03-13 18:26:04 +0100

    no message

--- app/backend/test.js
+++ app/backend/test.js
@@ -1,6 +1,51 @@
-const Remarkable = require("remarkable")
+const { Remarkable }  = require("remarkable")
 const fs = require('fs')
 
-console.log(fs.readFileSync("/Users/d023280/Documents/workspace/brainbox/app/shapes/circuit/display/7Segment.md", "utf8"))
+let md = fs.readFileSync("test.md", "utf8")
+
+let defaults = {
+  html: true,             // Enable HTML tags in source
+  xhtmlOut: false,        // Use '/' to close single tags (<br />)
+  breaks: false,          // Convert '\n' in paragraphs into <br>
+  langPrefix: 'language-',// CSS language prefix for fenced blocks
+  linkify: true,          // autoconvert URL-like texts to links
+  linkTarget: '_blank',   // set target to open link in
+  typographer: true       // Enable smartypants and other sweet transforms
+}
+
+let markdownParser = new Remarkable('full', this.defaults)
 
 
+markdownParser = new Remarkable('full', {
+  html:         false,        // Enable HTML tags in source
+  xhtmlOut:     false,        // Use '/' to close single tags (<br />)
+  breaks:       false,        // Convert '\n' in paragraphs into <br>
+  langPrefix:   'language-',  // CSS language prefix for fenced blocks
+  linkify:      true,         // autoconvert URL-like texts to links
+  linkTarget:   '',           // set target to open link in
+
+  // Enable some language-neutral replacements + quotes beautification
+  typographer:  false,
+
+  // Double + single quotes replacement pairs, when typographer enabled,
+  // and smartquotes on. Set doubles to '«»' for Russian, '„“' for German.
+  quotes: '“”‘’',
+
+  // Highlighter function. Should return escaped HTML,
+  // or '' if input not changed
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (__) {}
+    }
+
+    try {
+      return hljs.highlightAuto(str).value;
+    } catch (__) {}
+
+    return ''; // use external default escaping
+  }
+});
+markdownParser.inline.validateLink = function () { return true; };
+console.log(markdownParser.render(md))
--- app/frontend/circuit/js/dialog/MarkdownDialog.js
+++ app/frontend/circuit/js/dialog/MarkdownDialog.js
@@ -1,4 +1,4 @@
-import Remarkable from "remarkable"
+import {Remarkable, utils} from "remarkable"
 
 export default class MarkdownDialog {
 
@@ -8,7 +8,6 @@ export default class MarkdownDialog {
       xhtmlOut: false,        // Use '/' to close single tags (<br />)
       breaks: false,          // Convert '\n' in paragraphs into <br>
       langPrefix: 'language-',// CSS language prefix for fenced blocks
-      linkify: true,          // autoconvert URL-like texts to links
       linkTarget: '_blank',   // set target to open link in
       typographer: true       // Enable smartypants and other sweet transforms
     }
@@ -16,8 +15,20 @@ export default class MarkdownDialog {
 
   show(markdown, version) {
     let markdownParser = new Remarkable('full', this.defaults)
+    markdownParser.inline.validateLink = this.validateLink
     $('#markdownDialog .html').html(markdownParser.render(markdown))
     $('#markdownDialog .version').html(version)
     $('#markdownDialog').modal('show')
+  }
+
+  validateLink(url) {
+    let BAD_PROTOCOLS = [ 'vbscript', 'javascript', 'file'];
+    let str = url.trim().toLowerCase();
+    // Care about digital entities "javascript&#x3A;alert(1)"
+    str = utils.replaceEntities(str);
+    if (str.indexOf(':') !== -1 && BAD_PROTOCOLS.indexOf(str.split(':')[0]) !== -1) {
+      return false;
+    }
+    return true;
   }
 }
